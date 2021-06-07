a00fdaa78e983c54f4cec855d1a584d9f9671232
NT001234\admin <dandanbraz98@hotmail.com>
2021-06-07 12:13:05 -0300

    Realizar configuracao do projeto, adicionar classes, Dtos e criando endpoints de detalhamento de Topicos

---
+++ forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -0,0 +1,17 @@
+package br.com.alura.forum.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.alura.forum.modelo.Topico;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long>{
+
+	List<Topico> findByCursoNome(String nomeCurso);
+	
+//	--Para criar o método e configurar manualmente a Query a ser realizada no Banco de Dados.
+//	@Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
+//	List<Topico> carregarPorNomeDoCurso(@Param("nomeCurso") String nomeCurso);
+	
+}
